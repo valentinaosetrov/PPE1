01f7a4bee9eb7e07cb5dc2a4dc43ca784409ad6d
valentinaosetrov <valentina.osetrov@gmail.com>
2022-11-16 16:56:48 +0100

    mise à jour exercice traitement url bash

--- traitement_url_base.sh
+++ traitement_url_base.sh
@@ -19,19 +19,37 @@ fi
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
 
-# curl -I $URL | grep HTTP | 
+# curl -I $URL | grep HTTP | cut -d ' ' -f 2
 
 # !!!!!!
 # ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
 # !!!!!!
 
 # modifier la ligne suivante pour créer effectivement du HTML
-echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
+
+echo "<html>
+<header>
+<meta charset="UTF-8"
+</header>
+<body>
+<table>
+
+<tr><th>n° ligne</th><th>code HTTP</th><th>URL</th></tr>" > $fichier_tableau
+
+
+#echo "Je dois devenir du code HTML à partir de la question 3" > $fichier_tableau
 
 lineno=1;
 
 while read -r line;
 do
+	URL=$line 
+	CODE_HTTP=$(curl -I $URL | grep HTTP | cut -d ' ' -f 2)
+	echo "<tr><th>$lineno</th><th>$CODE_HTTP</th><th>$URL</th></tr>" >> $fichier_tableau
 	echo "ligne $lineno: $line";
 	lineno=$((lineno+1));
 done < $fichier_urls
+
+echo "</table></body></html>
+
+
